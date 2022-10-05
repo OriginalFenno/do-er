b8ffe43bb7664e1e63c446a02f752216fa764535
TFenton <tom.fenton@groupe-atlantic.co.uk>
2022-10-05 16:12:42 +0100

    Version 1.0.0 - bump deps; fixes compile warnings

---
+++ changes.md
@@ -0,0 +1,1 @@
+1.0.0  - Bump dependency versions; this fixes compile/runtime warnings from an early version of java-time
--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject OriginalFenno/do-er "0.1.1"
-  :dependencies [[clojure.java-time "0.3.2"]
-                 [org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "0.4.500"]]
+(defproject OriginalFenno/do-er "1.0.0"
+  :dependencies [[clojure.java-time "1.1.0"]
+                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/core.async "1.5.648"]]
   :repl-options {:init-ns do-er.core})
