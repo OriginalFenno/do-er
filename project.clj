73a71e0a0e8d4d180a3f762d3517464ee507172c
Tom Fenton <tafenton@icloud.com>
2022-10-05 18:32:01 +0100

    Version 1.0.4 - update project file

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
-(defproject org.clojars.tafenton/do-er "1.0.3"
+(defproject org.clojars.tafenton/do-er "1.0.4"
+  :description "A scheduling library inspired by my favourite bits of chime and overtone.at-at"
+  :url "https://github.com/tafenton/do-er"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[clojure.java-time "1.1.0"]
                  [org.clojure/clojure "1.11.1"]
                  [org.clojure/core.async "1.5.648"]]
